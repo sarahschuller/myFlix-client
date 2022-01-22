60eebe0bac6b7d4f77c3d013924020dd431e2b08
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-01-22 15:49:44 -0500

    revised prop names on MovieCard and MainView

--- src/dist/index.fcff31a1.js
+++ src/dist/index.fcff31a1.js
@@ -22973,7 +22973,7 @@ var _react = require("react");
 var _reactDefault = parcelHelpers.interopDefault(_react);
 class MovieCard extends _reactDefault.default.Component {
     render() {
-        const { movieData  } = this.props;
+        const { movie  } = this.props;
         return(/*#__PURE__*/ _jsxRuntime.jsx("div", {
             className: "movie-card",
             __source: {
