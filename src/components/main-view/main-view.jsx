f48a9ad3267a85c63b1c747386041a6236a0d31c
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-01-22 16:02:01 -0500

    added SelectedMovie state to main-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -22,6 +22,7 @@ export class MainView extends React.Component {
     if (selectedMovie) return <MovieView movie={selectedMovie} />;
   
     if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+  
     return (
       <div className="main-view">
         {movies.map(movie => <MovieCard key={movie._id} movie={movie}/>)}
