c26d563ced386ef54671fdff0adb86d22eb4c00a
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-01-22 20:00:43 -0500

    install and import axios, removed test movie array

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -7,11 +8,7 @@ export class MainView extends React.Component {
   constructor (){
       super();
       this.state = {
-          movies: [
-              {_id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...'},
-              {_id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'},
-              {_id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...'}
-          ],
+          movies: [],
           selectedMovie: null
       };
   }
