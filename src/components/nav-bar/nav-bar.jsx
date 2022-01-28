096c1ac0d1aab967b4f3f4b8c008b18b522c347e
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-01-28 12:42:41 -0500

    create nav-bar.jsx and import into main-view

--- src/components/nav-bar/nav-bar.jsx
+++ src/components/nav-bar/nav-bar.jsx
@@ -2,5 +2,23 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 
 export class NavBar extends React.Component {
-
 }
+
+
+<Navbar bg="light" expand="lg">
+  <Container fluid>
+    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+    <Navbar.Toggle aria-controls="navbarScroll" />
+    <Navbar.Collapse id="navbarScroll">
+      <Nav
+        className="me-auto my-2 my-lg-0"
+        style={{ maxHeight: '100px' }}
+        navbarScroll
+      >
+        <Nav.Link href="#action1">Home</Nav.Link>
+        <Nav.Link href="#action2">Link</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+
