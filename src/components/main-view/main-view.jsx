20f8b61e4e13598ba8159e77c08e6ccd5a63855f
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-02-06 14:14:41 -0500

    troubleshooting

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,7 +1,7 @@
 // React imports
 import React from 'react';
 import axios from 'axios';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // Bootstrap styling imports
 import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
@@ -25,6 +25,21 @@ export class MainView extends React.Component {
       };
   }
 
+  getMovies(token) {
+    axios.get('https://flixfile.herokuapp.com/movies', {
+      headers: { Authorization: `Bearer ${token}`}
+    })
+    .then(response => {
+      // Assign the result to the state
+      this.setState({
+        movies: response.data
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
   componentDidMount(){
     let accessToken = localStorage.getItem('token');
     if (accessToken !== null) {
@@ -35,90 +50,57 @@ export class MainView extends React.Component {
     }
   }
 
-  setSelectedMovie(movie){
+  // Existing user logs in
+  onLoggedIn(authData) {
+      console.log(authData);
       this.setState({
-          selectedMovie: movie
+        user: authData.user.Username
       });
+
+      localStorage.setItem('token', authData.token);
+      localStorage.setItem('user', authData.user.Username);
+      this.getMovies(authData.token);
   }
 
-    // Existing user logs in
-    onLoggedIn(authData) {
-        console.log(authData);
-        this.setState({
-          user: authData.user.Username
-        });
-
-        localStorage.setItem('token', authData.token);
-        localStorage.setItem('user', authData.user.Username);
-        this.getMovies(authData.token);
-    }
-
-    // Existing user logs out
-    onLoggedOut() {
-      localStorage.removeItem('token');
-      localStorage.removeItem('user');
-      this.setState({
-        user: null
-      });
-    }
-
-    getMovies(token) {
-      axios.get('https://flixfile.herokuapp.com/movies', {
-        headers: { Authorization: `Bearer ${token}`}
-      })
-      .then(response => {
-        // Assign the result to the state
-        this.setState({
-          movies: response.data
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    }
-  
+  // Existing user logs out
+  onLoggedOut() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    this.setState({
+      user: null
+    });
+  }
 
   render() {
-    const { movies, selectedMovie, user, registration } = this.state;
+    const { movies, user } = this.state;
 
-    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
-  
-    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+    if (!user) return <Row>
+      <Col>
+        <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+      </Col>
+    </Row>
+    
+    if (movies.length === 0) return <div className="main-view" />;
   
     return (
         <div className="main-view">    
-        <Navbar bg="primary" variant="dark" expand="lg">
-          <Container fluid>
-            <Navbar.Brand href="#">FlixFile</Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbarScroll" />
-            <Navbar.Collapse id="navbarScroll">
-              <Nav
-                className="me-auto my-2 my-lg-0"
-                style={{ maxHeight: '100px' }}
-                navbarScroll
-              >
-                <Nav.Link href="/index">Home</Nav.Link>
-                <Nav.Link href="#action2">Link</Nav.Link>
-                <Nav.Link href="/index" onClick={() => { this.onLoggedOut()}}>Logout</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+        <Router>
           <Row className="main-view justify-content-md-center">
-            {selectedMovie
-              ? (
-                <Col md={8}>
-                  <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
-                </Col>
-              )
-              : movies.map((movie) => (
-                <Col md={3} key={movie._id}>
-                  <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+            <Route exact path="/" render={() => {
+              return movies.map(m => (
+                <Col md={3} key={m._id}>
+                  <MovieCard movie={m} />
                 </Col>
               ))
-            }
+            }} />
+            <Route path="/movies/:movieId" render={({ match }) => {
+              return <Col md={8}>
+                <MovieView movie={movies.find(m => m._id === match.params.movieId)}/>
+              </Col>
+            }} />
           </Row>
-        </div>
+        </Router>
+        </div> 
       );
     };
 }
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,23 +1,27 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+import { Link } from "react-router-dom";
+
 export class MovieCard extends React.Component {
-    render() {
-        const { movie, onMovieClick } = this.props;
-        
-        return (
-            <Card>
-                <Card.Img variant="top" src={movie.ImagePath} crossOrigin="anonymous"/>
-                <Card.Body>
-                    <Card.Title>{movie.Title}</Card.Title>
-                    {/* <Card.Text>{movie.Description}</Card.Text> */}
-                    <Button variant="primary" onClick={() => onMovieClick(movie)} variant="link">Open</Button>
-                </Card.Body>
-            </Card>
-        );
-    }
+  render() {
+    const { movie } = this.props;
+
+    return (
+      <Card>
+        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>{movie.Description}</Card.Text>
+          <Link to={`/movies/${movie._id}`}>
+            <Button variant="link">Open</Button>
+          </Link>
+        </Card.Body>
+      </Card>
+    );
+  }
 }
 
 MovieCard.propTypes = {
